8bd1f0edbd101970aa4da8d151a7888e00565641
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-02 07:24:06 -0300

    Probando algo

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True' 
+DEBUG = TRUE 
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
