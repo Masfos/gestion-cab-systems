b26fa9d16b831e42a8523550a656e5662e8ace31
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-03 15:12:35 -0300

    Ajustes en diseño final de variados menús

--- core/forms.py
+++ core/forms.py
@@ -3,30 +3,20 @@ from .models import OrdenTrabajo, Cliente, Vehiculo, Material
 
 class OrdenTrabajoForm(forms.ModelForm):
     imagenes = forms.FileField(
-        widget=forms.FileInput(attrs={
-            'multiple': True, 
-            'class': 'form-control',
-            'accept': 'image/*'
-        }),
         required=False,
-        label="Adjuntar Fotos"
+        label="Adjuntar Fotos del Trabajo"
     )
 
     class Meta:
         model = OrdenTrabajo
         fields = ['vehiculo', 'descripcion', 'estado']
 
-class ClienteForm(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        fields = '__all__'
-
-class VehiculoForm(forms.ModelForm):
-    class Meta:
-        model = Vehiculo
-        fields = '__all__'
-
-class MaterialForm(forms.ModelForm):
-    class Meta:
-        model = Material
-        fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super(OrdenTrabajoForm, self).__init__(*args, **kwargs)
+        # Forzamos el uso de FileInput (que sí soporta multiple) 
+        # y eliminamos cualquier rastro de ClearableFileInput
+        self.fields['imagenes'].widget = forms.FileInput(attrs={
+            'multiple': True,
+            'class': 'form-control',
+            'accept': 'image/*'
+        })
--- core/views.py
+++ core/views.py
@@ -35,16 +35,14 @@ def editar_orden(request, orden_id):
             form.save()
             archivos = request.FILES.getlist('imagenes')
             for f in archivos:
+                from .models import ImagenOrden
                 ImagenOrden.objects.create(orden=orden, imagen=f)
-                
             return redirect("ver_orden", orden_id=orden.id)
     else:
         form = OrdenTrabajoForm(instance=orden)
     
-    return render(request, "editar_orden.html", {
-        "form": form, 
-        "orden": orden
-    })
+    return render(request, "editar_orden.html", {"form": form, "orden": orden})
+    
 @login_required
 def eliminar_orden(request, orden_id):
     if request.method == "POST" and es_admin(request.user):
