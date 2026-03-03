dfbd13183c5574577532794a9256038b78e23bfb
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-03 15:15:00 -0300

    Ajustes en diseño final de variados menús

--- core/forms.py
+++ core/forms.py
@@ -2,21 +2,33 @@ from django import forms
 from .models import OrdenTrabajo, Cliente, Vehiculo, Material
 
 class OrdenTrabajoForm(forms.ModelForm):
+    # Definimos el campo 'imagenes' de forma manual. 
+    # FileInput es SEGURO y no causa el crash de ValueError.
     imagenes = forms.FileField(
+        widget=forms.FileInput(attrs={
+            'multiple': True, 
+            'class': 'form-control',
+            'accept': 'image/*'
+        }),
         required=False,
-        label="Adjuntar Fotos del Trabajo"
+        label="Adjuntar Fotos"
     )
 
     class Meta:
         model = OrdenTrabajo
         fields = ['vehiculo', 'descripcion', 'estado']
 
-    def __init__(self, *args, **kwargs):
-        super(OrdenTrabajoForm, self).__init__(*args, **kwargs)
-        # Forzamos el uso de FileInput (que sí soporta multiple) 
-        # y eliminamos cualquier rastro de ClearableFileInput
-        self.fields['imagenes'].widget = forms.FileInput(attrs={
-            'multiple': True,
-            'class': 'form-control',
-            'accept': 'image/*'
-        })
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+class VehiculoForm(forms.ModelForm):
+    class Meta:
+        model = Vehiculo
+        fields = '__all__'
+
+class MaterialForm(forms.ModelForm):
+    class Meta:
+        model = Material
+        fields = '__all__'
--- core/views.py
+++ core/views.py
@@ -29,19 +29,25 @@ def ver_orden(request, orden_id):
 @login_required
 def editar_orden(request, orden_id):
     orden = get_object_or_404(OrdenTrabajo, id=orden_id)
+    
     if request.method == "POST":
         form = OrdenTrabajoForm(request.POST, request.FILES, instance=orden)
+        
         if form.is_valid():
             form.save()
             archivos = request.FILES.getlist('imagenes')
             for f in archivos:
                 from .models import ImagenOrden
                 ImagenOrden.objects.create(orden=orden, imagen=f)
+                
             return redirect("ver_orden", orden_id=orden.id)
     else:
         form = OrdenTrabajoForm(instance=orden)
     
-    return render(request, "editar_orden.html", {"form": form, "orden": orden})
+    return render(request, "editar_orden.html", {
+        "form": form, 
+        "orden": orden
+    })
     
 @login_required
 def eliminar_orden(request, orden_id):
