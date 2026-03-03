f6040266c7dca4cd7f474bd5cde0f51a022a27e4
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-03 14:23:46 -0300

    Ajustes en diseño final de variados menús

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import OrdenTrabajo, Cliente, Vehiculo, Material
 
 class OrdenTrabajoForm(forms.ModelForm):
-    # Esta línea es la que falta y la que está causando el error 500
     imagenes = forms.ImageField(
         widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': 'form-control'}),
         required=False,
@@ -20,7 +19,7 @@ class ClienteForm(forms.ModelForm):
 
 class VehiculoForm(forms.ModelForm):
     class Meta:
-        model = Cliente # Verifica si aquí debería ser Vehiculo, en tu código enviaste Cliente
+        model = Cliente
         fields = "__all__"
 
 class MaterialForm(forms.ModelForm):
