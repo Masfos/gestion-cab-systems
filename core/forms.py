9e3a89482e5e569f47241304cb462ae73e09acaa
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-03 14:26:54 -0300

    Ajustes en diseño final de variados menús

--- core/forms.py
+++ core/forms.py
@@ -10,19 +10,19 @@ class OrdenTrabajoForm(forms.ModelForm):
 
     class Meta:
         model = OrdenTrabajo
-        fields = "__all__"
+        fields = '__all__'
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = "__all__"
+        fields = '__all__'
 
 class VehiculoForm(forms.ModelForm):
     class Meta:
-        model = Cliente
-        fields = "__all__"
+        model = Vehiculo
+        fields = '__all__'
 
 class MaterialForm(forms.ModelForm):
     class Meta:
         model = Material
-        fields = "__all__"
+        fields = '__all__'
