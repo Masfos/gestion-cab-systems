957df616db5b80fe45a9cf59621df7ec74a7ff07
Christian <christian.aguilar@alumnos.ucn.cl>
2026-03-04 13:31:56 -0300

    feat: restaurar campo empresa opcional a modelo Cliente

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
